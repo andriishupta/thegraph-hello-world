a93b8ff0816b3e9668893f5fcd5bada2e7030820
Andrii Shupta <andrii.shupta@gmail.com>
2022-08-11 10:16:59 +0300

    :sparkles: finish example events

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Lens Protocol SubGraph - "Hello World"
 
 This project is created for testing Lens Protocol on TheGraph
+You can test API by visiting [GraphiQL Explorer](https://api.thegraph.com/subgraphs/name/andriishupta/hello-world/graphql?query=%7B%0A++profiles%28first%3A+5%29+%7B%0A++++id%0A++++handle%0A++++imageURI%0A++%7D%0A%7D)
 
 ## Development
 No simple way to have dev environment, so flow is: implement events and deploy to real HostedSubgraph
--- src/lens-hub.ts
+++ src/lens-hub.ts
@@ -1,36 +1,58 @@
+import { log } from '@graphprotocol/graph-ts';
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  Transfer as TransferEvent
-} from "../generated/LensHub/LensHub"
-import { Approval, ApprovalForAll, Transfer } from "../generated/schema"
+  PostCreated,
+  ProfileCreated,
+  ProfileImageURISet
+} from '../generated/LensHub/LensHub';
+import {
+  Profile,
+  Post
+} from '../generated/schema';
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-  entity.save()
+export function handleProfileCreated(event: ProfileCreated): void {
+  const profileId = event.params.profileId.toString();
+
+  log.info('Event [ProfileCreated] has been trigger for profileId: {}', [profileId]);
+
+  let profile = Profile.load(profileId);
+  if (profile === null) {
+    profile = new Profile(profileId);
+  }
+
+  profile.handle = event.params.handle;
+  profile.imageURI = profile.imageURI === null ? event.params.imageURI : profile.imageURI;
+  profile.save();
 }
 
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-  entity.save()
+export function handleProfileImageURISet(event: ProfileImageURISet): void {
+  const profileId = event.params.profileId.toString();
+
+  log.info('Event [ProfileImageURISet] has been called for profileId: {}', [profileId]);
+
+  let profile = Profile.load(profileId);
+  if (profile === null) {
+    profile = new Profile(profileId);
+  }
+
+  profile.imageURI = event.params.imageURI;
+  profile.save();
 }
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
+export function handlePostCreated(event: PostCreated): void {
+  const postId = event.params.profileId.toString();
+  log.info('Event [PostCreated] has been trigger for postId: {}', [postId]);
+  const post = new Post(postId);
+
+  const profileId = event.params.profileId.toString();
+  let profile = Profile.load(profileId);
+  if (profile === null) {
+    profile = new Profile(profileId);
+    profile.save();
+  }
+
+  post.profile = profile.id;
+  post.pubId = event.params.pubId.toString(); // todo: show be also changed like profile to link to publication
+  post.contentURI = event.params.contentURI;
+
+  post.save();
 }
